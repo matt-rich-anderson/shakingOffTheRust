e085b54376220c108d3c0562864047b8a81b3154
matt-rich-anderson <matt.rich.anderson@gmail.com>
2022-06-30 09:57:55 -0500

    additional practice

--- enchantedForest.js
+++ enchantedForest.js
@@ -4,7 +4,8 @@ const monsters = [
     {id: 2, name: "Dwarf", powerId: 3, battleCry: "Hide all you want, I'll still get you!"}, 
     {id: 3, name: "Imp", powerId: 2, battleCry: "I'll burn you all up!"}, 
     {id: 4, name: "Shoggoth", powerId: 5, battleCry: "Tekeli-li!"}, 
-    {id: 5, name: "Haunted Puppet", powerId: 4, battleCry: "I'll tell you secrets!"}
+    {id: 5, name: "Haunted Puppet", powerId: 4, battleCry: "I'll tell you secrets!"},
+    {id: 6, name: "Weather Wizard", powerId: 1, battleCry: "I can control the elements!"}
 ]
 
 const powers = [
@@ -12,7 +13,7 @@ const powers = [
     {id: 2, name: "Darkness"},
     {id: 3, name: "Wind"},
     {id: 4, name: "Void"},
-    {id: 5, name: "Ice"},
+    {id: 5, name: "Ice"}
 ]
 
 const identifyPower = (monsterName) => {
@@ -30,5 +31,11 @@ const addBleed = (bleedNum) => {
     return monsters.map((monster) => monster.bleed = bleedNum)
 }
 
-addBleed(20)
-console.log(monsters)
+const getAllMonstersByPower = (powerName) => {
+    const foundPower = powers.find((power) => power.name === powerName)
+    const foundMonsters = monsters.filter((monster) => monster.powerId === foundPower.id)
+    return foundMonsters
+}
+
+const lightningMonsters = getAllMonstersByPower("Lightning")
+console.log(lightningMonsters)
